639096a1b86dcb9098c47a821f2bc70c956f4530
Denis Gavrysh <gavden@gmail.com>
2025-03-26 17:35:38 +0200

    Simplify example

---
+++ inc/a2dp_sink_demo.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <inttypes.h>
+#include <stdint.h>
+
+#include "btstack.h"
+
+#define NUM_CHANNELS 2
+#define BYTES_PER_FRAME     (2*NUM_CHANNELS)
+#define MAX_SBC_FRAME_SIZE 120
+#define LOCAL_NAME "Raspberry Pi Pico W A2DP 00:00:00:00:00:00"
+
+// ring buffer for SBC Frames
+// below 30: add samples, 30-40: fine, above 40: drop samples
+#define OPTIMAL_FRAMES_MIN 60
+#define OPTIMAL_FRAMES_MAX 80
+#define ADDITIONAL_FRAMES  30
+
+typedef struct {
+    uint8_t  reconfigure;
+    uint8_t  num_channels;
+    uint16_t sampling_frequency;
+    uint8_t  block_length;
+    uint8_t  subbands;
+    uint8_t  min_bitpool_value;
+    uint8_t  max_bitpool_value;
+    btstack_sbc_channel_mode_t      channel_mode;
+    btstack_sbc_allocation_method_t allocation_method;
+} media_codec_configuration_sbc_t;
+
+typedef enum {
+    STREAM_STATE_CLOSED,
+    STREAM_STATE_OPEN,
+    STREAM_STATE_PLAYING,
+    STREAM_STATE_PAUSED,
+} stream_state_t;
+
+typedef struct {
+    uint8_t  a2dp_local_seid;
+    uint8_t  media_sbc_codec_configuration[4];
+} a2dp_sink_demo_stream_endpoint_t;
+
+typedef struct {
+    bd_addr_t addr;
+    uint16_t  a2dp_cid;
+    uint8_t   a2dp_local_seid;
+    stream_state_t stream_state;
+    media_codec_configuration_sbc_t sbc_configuration;
+} a2dp_sink_demo_a2dp_connection_t;
+
+
+typedef struct {
+    bd_addr_t addr;
+    uint16_t  avrcp_cid;
+    bool playing;
+    uint16_t notifications_supported_by_target;
+} a2dp_sink_demo_avrcp_connection_t;
